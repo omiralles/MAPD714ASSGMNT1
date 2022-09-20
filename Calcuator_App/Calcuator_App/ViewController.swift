22912176042aabb22d3be5f3b383be4bcbffb36c
omiralles <114028262+omiralles@users.noreply.github.com>
2022-09-20 17:00:52 -0400

    Add files via upload

---
+++ Calcuator_App/Calcuator_App/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  Calcuator_App
+//
+//  Created by Bahaa Al juboori on 2022-09-20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+
+        
+        // Student Name: Carlos Hernandez
+        // Student ID:
+        
+        // Student Name: Oscar
+        // Student ID:
+        
+        // Student Name: Bahaa Al-juboori
+        // Student ID: 3012869950
+        
+        
+    }
+
+
+}
+
