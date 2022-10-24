b3c7401af92facde4033a68ae7e421409119c624
omiralles <114028262+omiralles@users.noreply.github.com>
2022-10-23 20:47:05 -0400

    3rd assignment
    3rd assignment with landscape view and functions

--- Calcuator_App/Calcuator_App/ViewController.swift
+++ Calcuator_App/Calcuator_App/ViewController.swift
@@ -10,9 +10,6 @@
 // Student Name: Oscar Miralles Fernandez
 // Student ID: 301250756
 //
-// Student Name: Bahaa Al-juboori
-// Student ID: 3012869950
-//
 //This is a basic calculator app that incluides the operations +, -, *, / and %
 //the caculator accuracy it's 12 decimal places.
 
@@ -27,11 +24,13 @@ class ViewController: UIViewController {
     var resultLabelReady: Bool = true
     var result: Float = 0.0
     var activeOperator: String = ""
-    //Herarchy operations variables
-    var prevOperantHerarchy: Int = 0
-    var currentOperantHerarchy: Int = 0
-    var lastNumber: Float = 0.0
-    var lastOperator: String = ""
+    
+    //Stacks for operations, operands and memory
+    var leftOperatorsStack: [Float] = []
+    var rightOperatorsStack: [Float] = []
+    var operandsStack: [String] = []
+    //LIFO memory stack
+    var memoryStack: [Float] = []
     
     //Result Label
     @IBOutlet weak var ResultLabel: UILabel!
@@ -58,14 +57,97 @@ class ViewController: UIViewController {
         return lhs * rhs
     }
     //Divide function
-    func Divide(lhs: Float, rhs: Float)->Float
+    func Divide(lhs: Float, rhs: Float) -> Float
     {
         return lhs / rhs
     }
     //Percentage function
-    func Percentage(lhs: Float, rhs: Float)->Float
+    func Percentage(lhs: Float, rhs: Float) -> Float
     {
-        return (rhs / lhs) * 100
+        if (rhs != 0) {
+            return lhs * (rhs / 100)
+        }
+        else
+        {
+            return lhs / 100
+        }
+    }
+    //Sinus function
+    func Sinus(lhs: Float) -> Float
+    {
+        return (sin(lhs * Float(Double.pi) / 180))
+    }
+    //Cosinus function
+    func Cosinus(lhs: Float) -> Float
+    {
+        return (cos(lhs * Float(Double.pi) / 180))
+    }
+    //Tangent function
+    func Tangent(lhs: Float) -> Float
+    {
+        return (tan(lhs * Float(Double.pi) / 180))
+    }
+    //Sinh function
+    func Asinus(lhs: Float) -> Float
+    {
+        return (sinh(lhs))
+    }
+    //Cosh function
+    func Acosinus(lhs: Float) -> Float
+    {
+        return (cosh(lhs))
+    }
+    //Tanh function
+    func Atangent(lhs: Float) -> Float
+    {
+        return (tanh(lhs))
+    }
+    //Logarithm function
+    func Logarithm(lhs: Float) -> Float
+    {
+        return (log10(lhs))
+    }
+    //Logarithm natural function
+    func LogarithmN(lhs: Float) -> Float
+    {
+        return (log(lhs))
+    }
+    //Square function
+    func Square(lhs: Float) -> Float
+    {
+        return (pow(lhs,2))
+    }
+    //Square 3 function
+    func SquareThree(lhs: Float) -> Float
+    {
+        return (pow(lhs,3))
+    }
+    //Power function
+    func Power(lhs: Float, rhs: Float) -> Float
+    {
+        if (rhs != 0) {
+            return (pow(lhs,rhs))
+        }
+        else
+        {
+            return(lhs)
+        }
+    }
+    //Square root function
+    func SquareR(lhs: Float) -> Float
+    {
+        return (sqrtf(lhs))
+    }
+    //Power root function
+    func PowerRoot(lhs: Float, rhs: Float) -> Float
+    {
+        if (rhs != 0) {
+            return (pow(lhs,1/rhs))
+        }
+        else
+        {
+            return(lhs)
+        }
     }
     
     //Format de result to appropiate string and decimal places
@@ -101,6 +183,32 @@ class ViewController: UIViewController {
             result = Divide(lhs: leftOperand, rhs: rightOperand)
         case "%":
             result = Percentage(lhs: leftOperand, rhs: rightOperand)
+        case "sin":
+            result = Sinus(lhs: leftOperand)
+        case "cos":
+            result = Cosinus(lhs: leftOperand)
+        case "tan":
+            result = Tangent(lhs: leftOperand)
+        case "sinh":
+            result = Asinus(lhs: leftOperand)
+        case "cosh":
+            result = Acosinus(lhs: leftOperand)
+        case "tanh":
+            result = Atangent(lhs: leftOperand)
+        case "log":
+            result = Logarithm(lhs: leftOperand)
+        case "ln":
+            result = LogarithmN(lhs: leftOperand)
+        case "sq2":
+            result = Square(lhs: leftOperand)
+        case "sq3":
+            result = SquareThree(lhs: leftOperand)
+        case "sqy":
+            result = Power(lhs: leftOperand, rhs: rightOperand)
+        case "sqr":
+            result = SquareR(lhs: leftOperand)
+        case "sqry":
+            result = PowerRoot(lhs: leftOperand, rhs: rightOperand)
         default:
             result = 0.0
         }
@@ -112,6 +220,9 @@ class ViewController: UIViewController {
         haveRightOperand = false
         resultLabelReady = false
         activeOperator = ""
+        
+        //Add result to left operant stack
+        leftOperatorsStack.append(leftOperand)
         
         //Format the result to appropiate string
         resultStr = String(result)
@@ -128,17 +239,114 @@ class ViewController: UIViewController {
             herarchy = 2
         case "-":
             herarchy = 2
-        case "x":
+        case "X":
             herarchy = 1
-        case "÷":
+        case "/":
             herarchy = 1
-        case "%":
+        case "sqy":
+            herarchy = 1
+        case "sqry":
             herarchy = 1
         default:
-            herarchy = prevOperantHerarchy
+            herarchy = 0
         }
         
         return herarchy
+    }
+    
+    //Shunting yard algorithm variant
+    //Stack evaluation with priorities
+    //If we have same herarchy operations we have one operation in out stack. Otherwise, we have two operations
+    func DecisionStack () -> Bool{
+        //One operant in stack decision
+        if (operandsStack.count == 1) {
+            if (OperantHerarchy(value: operandsStack[0]) == 0) {
+                leftOperand = leftOperatorsStack[0]
+                rightOperand = 0.0
+                activeOperator = operandsStack[0]
+                Evaluate()
+                rightOperand = 0.0
+                leftOperand = 0.0
+                haveLeftOperand = false
+                haveRightOperand = false
+                operandsStack.removeAll()
+                leftOperatorsStack.removeAll()
+                rightOperatorsStack.removeAll()
+            }
+            else {
+                leftOperand = leftOperatorsStack[0]
+                rightOperand = rightOperatorsStack[0]
+                activeOperator = operandsStack[0]
+                Evaluate()
+                leftOperand = result
+                rightOperand = 0.0
+                haveLeftOperand = false
+                haveRightOperand = false
+                operandsStack.removeAll()
+                leftOperatorsStack.removeAll()
+                rightOperatorsStack.removeAll()
+            }
+        }
+        else {
+            //Mora than one operant decision
+            //In the event that we have pending operations of equal herarchy
+            if (operandsStack.count > 1) {
+                if (OperantHerarchy(value: operandsStack[0]) ==
+                    OperantHerarchy(value: operandsStack[1])) {
+                    leftOperand = leftOperatorsStack[0]
+                    rightOperand = rightOperatorsStack[0]
+                    activeOperator = operandsStack[0]
+                    Evaluate()
+                    operandsStack.removeFirst()
+                    leftOperatorsStack.removeFirst()
+                    rightOperatorsStack.removeFirst()
+                    leftOperand = result
+                    rightOperand = 0.0
+                }
+                else {
+                    //In the event that we have pending operations of 0 hierarchy
+                    if (OperantHerarchy(value: operandsStack[1]) == 0)
+                    {
+                        leftOperand = leftOperatorsStack[0]
+                        rightOperand = rightOperatorsStack[0]
+                        activeOperator = operandsStack[1]
+                        Evaluate()
+                        rightOperatorsStack[0] = result
+                        leftOperatorsStack.removeLast()
+                        rightOperatorsStack.removeLast()
+                        operandsStack.remove(at: 1)
+                        leftOperand = result
+                        rightOperand = 0.0
+                        return true
+                    }
+                    else
+                    {
+                        //Diferent herarchy cases. Herarchy 1 cases
+                        if (rightOperatorsStack.count > 1){
+                            leftOperand = rightOperatorsStack[0]
+                            rightOperand = rightOperatorsStack[1]
+                            activeOperator = operandsStack[1]
+                            Evaluate()
+                            rightOperatorsStack[0] = result
+                            leftOperatorsStack.remove(at: 1)
+                            operandsStack.remove(at: 1)
+                            rightOperatorsStack.remove(at: 1)
+                            rightOperand = result
+                            leftOperand = 0.0
+                        }
+                    }
+                }
+            }
+        }
+        
+        //Recall in case of pending operations in the stack with same herarchy
+        if ((operandsStack.count > 1) &&
+            (OperantHerarchy(value: operandsStack[0]) ==
+            OperantHerarchy(value: operandsStack[1]))){
+            resultLabelReady = DecisionStack()
+        }
+    
+        return false
     }
     
     // Handlers operation pressed
@@ -151,6 +359,10 @@ class ViewController: UIViewController {
         if(!haveLeftOperand)
         {
             leftOperand = Float(resultLabelText!)!
+            
+            //Add Left stack
+            leftOperatorsStack.append(Float(resultLabelText!)!)
+            
             haveLeftOperand = true
             resultLabelReady = false
         }
@@ -158,47 +370,14 @@ class ViewController: UIViewController {
         {
             if (resultLabelText != "0") {
                 rightOperand = Float(resultLabelText!)!
+                
+                //Add Right stack
+                rightOperatorsStack.append(Float(resultLabelText!)!)
+                
                 haveRightOperand = true
             }
             else {
                 haveRightOperand = false
-            }
-        }
-        
-        //Herarcy decisions based on operation
-        if (prevOperantHerarchy == 0) {
-            prevOperantHerarchy = OperantHerarchy(value: currentInput!)
-        }
-        else {
-            currentOperantHerarchy = OperantHerarchy(value: currentInput!)
-        }
-        
-        if(haveLeftOperand && haveRightOperand && resultLabelReady)
-        {
-            //different behaviors based on the hierarchy of operations
-            if (currentOperantHerarchy < prevOperantHerarchy) {
-                lastNumber = leftOperand
-                lastOperator = activeOperator
-                leftOperand = rightOperand
-                rightOperand = 0.0
-                resultLabelReady = false
-            }
-            else {
-                if (lastNumber != 0){
-                    //Evaluate and restore the last operation values
-                    Evaluate()
-                    rightOperand = result
-                    leftOperand = lastNumber
-                    activeOperator = lastOperator
-                }
-                //Evaluate the las operation and clean it
-                Evaluate()
-                leftOperand = result
-                rightOperand = 0.0
-                lastNumber = 0.0
-                lastOperator = ""
-                prevOperantHerarchy = currentOperantHerarchy
-                resultLabelReady = false
             }
         }
         
@@ -214,15 +393,55 @@ class ViewController: UIViewController {
             activeOperator = "/"
         case "%":
             activeOperator = "%"
+        case "sin":
+            activeOperator = "sin"
+        case "cos":
+            activeOperator = "cos"
+        case "tan":
+            activeOperator = "tan"
+        case "sinh":
+            activeOperator = "sinh"
+        case "cosh":
+            activeOperator = "cosh"
+        case "tanh":
+            activeOperator = "tanh"
+        case "log":
+            activeOperator = "log"
+        case "ln":
+            activeOperator = "ln"
+        case "sq2":
+            activeOperator = "sq2"
+        case "sq3":
+            activeOperator = "sq3"
+        case "sqy":
+            activeOperator = "sqy"
+        case "sqr":
+            activeOperator = "sqr"
+        case "sqry":
+            activeOperator = "sqry"
         case "=":
             if (haveLeftOperand && haveRightOperand) {
-                Evaluate()
-                leftOperand = result
-                rightOperand = 0.0
+                while (!operandsStack.isEmpty) {
+                    resultLabelReady = DecisionStack()
+                }
+                activeOperator = ""
                 resultLabelReady = false
             }
         default:
             print("Error!")
+        }
+        
+        //Add operator stack
+        if (activeOperator != ""){
+            operandsStack.append(activeOperator)
+        }
+        
+        if (!operandsStack.isEmpty) {
+            if((haveLeftOperand && haveRightOperand && resultLabelReady) ||
+               OperantHerarchy(value: operandsStack[0]) == 0)
+            {
+                resultLabelReady = DecisionStack()
+            }
         }
     }
     
@@ -262,7 +481,8 @@ class ViewController: UIViewController {
         }
     }
     
-    //Handle delete button and clear all button
+    //Handle delete button and clear all button and special operations
+    //in extended version
     @IBAction func ExtraButton_Pressed(_ sender: UIButton)
     {
         let button = sender as UIButton
@@ -276,11 +496,31 @@ class ViewController: UIViewController {
             haveLeftOperand = false
             haveRightOperand = false
             activeOperator = ""
-            //Clear herarchy variables
-            prevOperantHerarchy = 0
-            currentOperantHerarchy = 0
-            lastNumber = 0.0
-            lastOperator = ""
+            //Clear stacks
+            leftOperatorsStack = []
+            rightOperatorsStack = []
+            operandsStack = []
+        case "chs":
+            //Change the calcul sign
+            ResultLabel.text = FormatValue(value: String(Float(ResultLabel.text!)! * -1))
+        case "mc":
+            //Memory stack clear
+            if (!memoryStack.isEmpty) { memoryStack.removeAll() }
+        case "m+":
+            //Memory stack append
+            memoryStack.append(Float(ResultLabel.text!)!)
+        case "m-":
+            //Memory stack delete element
+            if (!memoryStack.isEmpty) { memoryStack.removeLast() }
+        case "mr":
+            //Memory stack retrieve
+            if (!memoryStack.isEmpty) {
+                ResultLabel.text = FormatValue(value: String(memoryStack[memoryStack.count - 1]))
+            }
+        case "e":
+            ResultLabel.text = FormatValue(value: String(2.718281828459045))
+        case "π":
+            ResultLabel.text = FormatValue(value: String(Double.pi))
         default:
             if(ResultLabel.text!.count > 1)
             {
